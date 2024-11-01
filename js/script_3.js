12d02e0c97b484f2b9f5d0d4e60ba3b3691393d7
Arsenii <supratuner@mail.ru>
2024-11-02 03:51:35 +0700

    Version 1.5
    Comments

--- js/script_3.js
+++ js/script_3.js
@@ -1,31 +1,31 @@
-document.getElementById("send_3").onclick = exercise_3;
-function exercise_3(){
-    let x = Number(document.getElementById("name_3").value); 
-    let img = document.getElementById("exercise_image_3");
-    let y;
-    const a = 2.6;
-    const b = 5.1;
-    if (isNaN(x)){
-        alert("Введите ещё раз корректное число");
-    } else {
-        if (x <= 1){
-            y = a * (Math.cos(x) ** 2) - b * Math.sin(x ** 2);
-            img.src = '../assets/exercise_3_1.png';
+document.getElementById("send_3").onclick = exercise_3; //Переход по клику
+function exercise_3(){ //Объявление функции
+    let x = Number(document.getElementById("name_3").value); //Получение переменной по id
+    let img = document.getElementById("exercise_image_3"); //Объявление переменной для картинки
+    let y; //Объявление переменной для уравнения
+    const a = 2.6; //Объявление константы
+    const b = 5.1; //Объявление константы
+    if (isNaN(x)){ //Объявление условия на проверку
+        alert("Введите ещё раз корректное число"); //Вывод о несоответствии введенных данных
+    } else {//Если все верно, тогда
+        if (x <= 1){ //Объявление условия на проверку
+            y = a * (Math.cos(x) ** 2) - b * Math.sin(x ** 2); //Объявление уравнения
+            img.src = '../assets/exercise_3_1.png'; //Присваивание номера картинки
             document.getElementById("output_exercise_3").innerHTML = `Значение X ${x}
-                \nЗначение Y ${y}`;
-            return;
-        } else if ((x > 1) && (x <= 4)){
-            y = b * Math.log(x) + x ** 3;
-            img.src = '../assets/exercise_3_2.png';
+                \nЗначение Y ${y}`; //Вывод по id
+            return; //Возвращение функции
+        } else if ((x > 1) && (x <= 4)){ //Объявление условия на проверку
+            y = b * Math.log(x) + x ** 3; //Объявление уравнения
+            img.src = '../assets/exercise_3_2.png';//Присваивание номера картинки
             document.getElementById("output_exercise_3").innerHTML = `Значение X ${x}
-                \nЗначение Y ${y}`;
-            return;
-        } else {
-            y = Math.sqrt((x ** 2) + a * b);
-            img.src = '../assets/exercise_3_3.png';
+                \nЗначение Y ${y}`; //Вывод по id
+            return;//Возвращение функции
+        } else { //Если не прошел по предыдущим условиям, тогда
+            y = Math.sqrt((x ** 2) + a * b); //Объявление уравнения
+            img.src = '../assets/exercise_3_3.png';//Присваивание номера картинки
             document.getElementById("output_exercise_3").innerHTML = `Значение X ${x}
-                \nЗначение Y ${y}`;
-            return;
+                \nЗначение Y ${y}`; //Вывод по id
+            return; //Возвращение функции
         }
         }
 }
--- js/script_4.js
+++ js/script_4.js
@@ -1,34 +1,34 @@
-document.getElementById("send_4").onclick = exercise_4;
-function exercise_4(){
-    let x = Number(document.getElementById("name_4").value); 
-    let img = document.getElementById("exercise_image_4");
-    let y;
-    const a = 2.6;
-    const b = 5.1;
-    if (isNaN(x)){
-        alert("Введите ещё раз корректное число");
-    } else {
-        switch (x) {
-            case 1:
-                y = a * (Math.cos(x) ** 2) - b * Math.sin(x ** 2);
+document.getElementById("send_4").onclick = exercise_4; //Переход по клику
+function exercise_4(){ //Объявление функции
+    let x = Number(document.getElementById("name_4").value); //Получение переменной по id
+    let img = document.getElementById("exercise_image_4"); //Объявление переменной для картинки
+    let y; //Объявление переменной для уравнения
+    const a = 2.6; //Объявление константы
+    const b = 5.1; //Объявление константы
+    if (isNaN(x)){ //Объявление условия на проверку
+        alert("Введите ещё раз корректное число"); //Вывод о несоответствии введенных данных
+    } else { //Если все верно, тогда
+        switch (x) { //Подключение функции переключения
+            case 1: //Первый вариант при Х = 1
+                y = a * (Math.cos(x) ** 2) - b * Math.sin(x ** 2); //Объявление уравнения
                 document.getElementById("output_exercise_4").innerHTML =`Значение X ${x} 
-                    \nЗначение Y ${y}`;
-                img.src = '../assets/exercise_4_1.png';
-                break;
-            case 4:
-                y = b * Math.log(x) + x ** 3;
+                    \nЗначение Y ${y}`; //Вывод по id
+                img.src = '../assets/exercise_4_1.png'; //Присваивание номера картинки
+                break; //Остановка
+            case 4: //Второй вариант при Х = 4
+                y = b * Math.log(x) + x ** 3; //Объявление уравнения
                 document.getElementById("output_exercise_4").innerHTML =`Значение X ${x} 
-                    \nЗначение Y ${y}`;
-                img.src = '../assets/exercise_4_2.png';
-                break;
-            case 5:
-                y = Math.sqrt((x ** 2) + a * b);
+                    \nЗначение Y ${y}`; //Вывод по id
+                img.src = '../assets/exercise_4_2.png'; //Присваивание номера картинки
+                break; //Остановка
+            case 5: //Третий вариант при Х = 5
+                y = Math.sqrt((x ** 2) + a * b); //Объявление уравнения
                 document.getElementById("output_exercise_4").innerHTML =`Значение X ${x} 
-                    \nЗначение Y ${y}`;
-                img.src = '../assets/exercise_4_3.png';
-                break;
-            default:
-                document.getElementById("output_exercise_4").innerHTML = "Для введенного значения X невозможно найти Y";
+                    \nЗначение Y ${y}`; //Вывод по id
+                img.src = '../assets/exercise_4_3.png'; //Присваивание номера картинки
+                break; //Остановка
+            default: //Если не прошел ни по одному кейсу
+                document.getElementById("output_exercise_4").innerHTML = "Для введенного значения X невозможно найти Y"; //Вывод по id
         }
     }
 }
